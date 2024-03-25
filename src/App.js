e57a300dcf1f73ce1ccf27d3d505ac33dc90bfce
Levan Iordanishvili <levandev9053@gmail.com>
2024-03-25 14:58:08 -0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
-
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import Home from './Home'
 
 export default function App() {
   return (
     <Router>
       <Navbar bg="dark" data-bs-theme="dark" style={{ marginBottom: "2vh" }}>
+        <Container>
+          <Navbar.Brand href="/">Energy Resources</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/add">
+              <Button variant="outline-primary" className="float-right">
+                Add Resource
+              </Button>
+            </Nav.Link>
+          </Nav>
+        </Container>
       </Navbar>
       <Routes>
         <Route exact path="/" element={<Home />} />
